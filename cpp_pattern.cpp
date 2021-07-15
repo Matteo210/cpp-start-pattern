58ca7963d64a0eb0fbbe11dd8a5bf825974f3a25
Matteo210 <70132748+Matteo210@users.noreply.github.com>
2021-07-15 18:50:21 +0200

    Add files via upload

---
+++ cpp_pattern.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+	string a;
+	cout << "Welcome \n";
+	
+	
+	
+	cin >> a;
+	cout << "\n";
+	Sleep(1);
+	cout << "program finished \n";
+	return 0;
+}
